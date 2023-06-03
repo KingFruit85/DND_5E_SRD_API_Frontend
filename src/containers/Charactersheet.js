8a1e062c1bd584f06f1215fb020e4e56bd2e9e69
Christopher Long <36999273+KingFruit85@users.noreply.github.com>
2023-06-03 21:20:15 +0100

    config

--- src/containers/Charactersheet.js
+++ src/containers/Charactersheet.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import AttackAndSpellcastingTable from "./AttackAndSpellcastingTable";
 import Equipment from "./Equipment";
 
--- src/containers/Equipment.js
+++ src/containers/Equipment.js
@@ -1,33 +1,32 @@
-function Equipment({character})
-{
-    let equipment = "";
-    console.log(character);
-    if (character.ClassDetails.Armor.Name !== null) {
-        equipment += character.ClassDetails.Armor.Name + ", ";
-    }
-    if (character.ClassDetails.Shield !== null) {
-        equipment += character.ClassDetails.Shield.Name + ", ";
-    }
-    if (character.ClassDetails.Ammunition.length > 0) {
-        character.ClassDetails.Ammunition.forEach((item) => {
-            equipment += `${item.Count} x ${item.Name}, `;
-        })
-    }
-    if (character.ClassDetails.MusicalInstruments.length > 0) {
-        character.ClassDetails.MusicalInstruments.forEach((item) => {
-            equipment += item.Name +", ";
-        })
-    }
-    if (character.ClassDetails.EquipmentPack !== null) {
-        equipment += character.ClassDetails.EquipmentPack.Name + ", ";
-    }
-    if (character.ClassDetails.HolySymbol !== null) {
-        equipment += character.ClassDetails.HolySymbol.Name + ", ";
-    }
+import React from "react";
 
-    return(
-        <textarea>{equipment.slice(0,-2)}</textarea>
-    )
+function Equipment({ character }) {
+  let equipment = "";
+  console.log(character);
+  if (character.ClassDetails.Armor.Name !== null) {
+    equipment += character.ClassDetails.Armor.Name + ", ";
+  }
+  if (character.ClassDetails.Shield !== null) {
+    equipment += character.ClassDetails.Shield.Name + ", ";
+  }
+  if (character.ClassDetails.Ammunition.length > 0) {
+    character.ClassDetails.Ammunition.forEach((item) => {
+      equipment += `${item.Count} x ${item.Name}, `;
+    });
+  }
+  if (character.ClassDetails.MusicalInstruments.length > 0) {
+    character.ClassDetails.MusicalInstruments.forEach((item) => {
+      equipment += item.Name + ", ";
+    });
+  }
+  if (character.ClassDetails.EquipmentPack !== null) {
+    equipment += character.ClassDetails.EquipmentPack.Name + ", ";
+  }
+  if (character.ClassDetails.HolySymbol !== null) {
+    equipment += character.ClassDetails.HolySymbol.Name + ", ";
+  }
+
+  return <textarea>{equipment.slice(0, -2)}</textarea>;
 }
 
 export default Equipment;
